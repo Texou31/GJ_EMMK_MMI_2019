dcadf20aa26c864de093e2d22c8e9190672c3efe
Texou31 <texou31@gmail.com>
2019-01-26 00:08:56 +0100

    New inputs
    Define the inputs axes and test them.
    Two tests prefabs to visualise the inputs and the axes.

---
+++ Assets/Scripts/Test/TestInput.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInput : MonoBehaviour
+{
+    public float horizontal1;
+    public float vertical1;
+
+    public float horizontal2;
+    public float vertical2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal1 = Input.GetAxis("Horizontal1");
+        vertical1 = Input.GetAxis("Vertical1");
+
+        horizontal2 = Input.GetAxis("Horizontal2");
+        vertical2 = Input.GetAxis("Vertical2");
+
+        if (Input.GetButtonDown("Interact1"))
+        {
+            Debug.Log("Interact 1");
+        }
+        if (Input.GetButtonDown("Interact2"))
+        {
+            Debug.Log("Interact 2");
+        }
+        if (Input.GetButtonDown("Submit"))
+        {
+            Debug.Log("Submit");
+        }
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Debug.Log("Cancel");
+        }
+        if (Input.GetButtonDown("Pause"))
+        {
+            Debug.Log("Pause");
+        }
+    }
+}
---
+++ Assets/Scripts/Test/VisuelJoystick.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisuelJoystick : MonoBehaviour
+{
+    [Range(1, 2)]
+    public int player;
+
+    void OnDrawGizmosSelected()
+    {
+        Vector3 target = new Vector3();
+        if(player == 1)
+        {
+            target.x = Input.GetAxis("Horizontal1");
+            target.y = Input.GetAxis("Vertical1");
+        } else if (player == 2)
+        {
+            target.x = Input.GetAxis("Horizontal2");
+            target.y = Input.GetAxis("Vertical2");
+        } else
+        {
+            Debug.Log("Bug");
+        }
+        
+
+        if (target != null)
+        {
+            // Draws a blue line from this transform to the target
+            
+            if (player == 1)
+            {
+                Gizmos.color = Color.blue;
+            }
+            else if(player == 2)
+            {
+                Gizmos.color = Color.green;
+            } else
+            {
+                Gizmos.color = Color.red;
+            }
+            Gizmos.DrawLine(transform.position, target + transform.position);
+        }
+    }
+}
