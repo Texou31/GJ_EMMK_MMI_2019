19b200c28bca89666b259b214bec94eb15fc0736
Cerdcee <hel.bal.let@gmail.com>
2019-01-26 11:23:04 +0100

    Carpet can finally be moved

--- Assets/TapisController.cs
+++ Assets/TapisController.cs
@@ -4,25 +4,38 @@ using UnityEngine;
 
 public class TapisController : MonoBehaviour
 {
-    private bool isHold;
+    private bool isHeld;
     private GameObject holder;
+
+    private Vector2 rotation = Vector2.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        isHold = false;
+        isHeld = false;
         holder = null;
+    }
+
+    void Update()
+    {
+        if (isHeld)
+        {
+            setPositionOffset();
+        }
     }
 
     public void Interact(GameObject newHolder)
     {
-        if (!isHold)
+        if (!isHeld)
         {
             SetNewHolder(newHolder);
             return;
         }
+    }
 
-        if(newHolder == holder)
+    public void StopInteract()
+    {
+        if (holder != null)
         {
             UnsetHolder();
         }
@@ -30,7 +43,7 @@ public class TapisController : MonoBehaviour
 
     private void SetNewHolder(GameObject newHolder)
     {
-        isHold = true;
+        isHeld = true;
         holder = newHolder;
 
         transform.SetParent(holder.transform);
@@ -38,9 +51,33 @@ public class TapisController : MonoBehaviour
 
     private void UnsetHolder()
     {
-        isHold = false;
+        isHeld = false;
         holder = null;
 
         transform.SetParent(null);
+    }
+
+    private void setPositionOffset()
+    {
+        PlayerMovement holderMovement = holder.GetComponent<PlayerMovement>();
+        switch (holderMovement.getDirection())
+        {
+            case "up":
+                transform.eulerAngles = new Vector3(0, 0, 90);
+                transform.localPosition = new Vector3(0, 1.5f, 0);
+                break;
+            case "down":
+                transform.eulerAngles = new Vector3(0, 0, 90);
+                transform.localPosition = new Vector3(0, -1.5f, 0);
+                break;
+            case "right":
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                transform.localPosition = new Vector3(1.5f, 0, 0);
+                break;
+            default: // right/left
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                transform.localPosition = new Vector3(-1.5f, 0, 0);
+                break;
+        }
     }
 }
