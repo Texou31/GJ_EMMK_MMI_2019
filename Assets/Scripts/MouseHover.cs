8391c2c744d1dca87bb8f2977785725d286c4ef2
Cerdcee <hel.bal.let@gmail.com>
2019-01-26 15:00:32 +0100

    Functionnal hideous menu

---
+++ Assets/Scripts/MouseHover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+    Renderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        renderer.material.color = Color.black;
+    }
+
+    void OnMouseEnter()
+    {
+        renderer.material.color = Color.red;
+    }
+
+    void OnMouseExit()
+    {
+        renderer.material.color = Color.black;
+    }
+}
