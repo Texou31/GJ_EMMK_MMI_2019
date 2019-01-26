71858500e4f147d705a0adf88d9280536424cb74
Texou31 <texou31@gmail.com>
2019-01-26 16:38:19 +0100

    First commit for the scene manager
    Add a scene manager who can call the others scenes needed.

---
+++ Assets/Scripts/GestionScene/SceneController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public string nextSceneName;
+
+    private string menuSceneName = "GIVE ME A REAL NAME"; //TODO 
+
+    private static SceneController instance;
+
+    void Start()
+    {
+        instance = this;
+    }
+
+    public void GoToMenu()
+    {
+        Debug.Log("Mauvaise valeur de variable car la scene n'a pas été définie");
+        // SceneManager.LoadScene(SceneManager.GetSceneByName(menuSceneName).buildIndex);
+    }
+
+    public void GoToNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetSceneByName(nextSceneName).buildIndex);
+    }
+
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
+
+
