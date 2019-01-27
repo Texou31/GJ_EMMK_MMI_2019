6e276196c46dea2a03070c1c3b8830b3bc17b722
Texou31 <texou31@gmail.com>
2019-01-27 17:31:10 +0100

    Cinematics !

---
+++ Assets/CinematicController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class CinematicController : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(videoPlayer.frame >= (long)videoPlayer.frameCount)
+        {
+            SceneManager.LoadScene("MenuScene");
+        }
+    }
+}
